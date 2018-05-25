e7e4aadfaf757d6fb4d9d58adcbfe49136a19fd2
Sascha Rodekamp <rodekamp@smartsquare.de>
2018-05-25 18:50:15 +0200

    add simple webcam contorller

--- src/main/kotlin/com/rdk/rekognition/controller/ImageController.kt
+++ src/main/kotlin/com/rdk/rekognition/controller/ImageController.kt
@@ -3,6 +3,8 @@ package com.rdk.rekognition.controller
 import com.amazonaws.services.rekognition.model.FaceDetail
 import com.rdk.rekognition.service.ReKognitionService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,6 +12,11 @@ class ImageController(val service: ReKognitionService) {
 
     @GetMapping("/imageinfo")
     fun index(): List<FaceDetail> {
+        return service.detectFaces()
+    }
+
+    @PostMapping("processwebcam")
+    fun processWebCam(@RequestParam() imageBlob: Byte ): List<FaceDetail>  {
         return service.detectFaces()
     }
 
